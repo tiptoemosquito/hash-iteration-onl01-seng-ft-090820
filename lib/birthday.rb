fc50115661f95728d2900b291acc143d322f4d2e
Christina Howard <clhoward33@icloud.com>
2020-09-14 01:03:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,10 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end# add your code snippet here!
 end
+
+def birthday_kids(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if > 9
+      puts "Happy Birthday #{kinds_name}! You are too old for this."
+    end
+  end 
